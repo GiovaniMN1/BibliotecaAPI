22da1f8253faabf371da63dbe87548a63bd76689
Giovani Madero <gioaldo172@gmail.com>
2025-06-29 21:35:42 -0600

    Se agrega metodos de tipo GET y POST para obtener agregar y leer informacion de la DB

--- BibliotecaAPI/Controllers/AutoresController.cs
+++ BibliotecaAPI/Controllers/AutoresController.cs
@@ -1,20 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using BibliotecaAPI.Entidades;
+using BibliotecaAPI.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace BibliotecaAPI.Controllers
 {
     [ApiController]
     [Route("api/autores")]
-    public class AutoresController
+    public class AutoresController:ControllerBase
     {
-        [HttpGet]
-        public IEnumerable<Autor> Get()
+        private readonly ApplicationDbContext context;
+        //Se agrega el constructor para permitir la inyección de dependencias DbContext
+        public AutoresController(ApplicationDbContext context)
         {
-            return new List<Autor>
-            {
-                new Autor { Id = 1, Name = "Autor 1" },
-                new Autor { Id = 2, Name = "Autor 2" }
-            };  
+            this.context = context;
         }
+        //[HttpGet]
+        //public IEnumerable<Autor> Get()
+        //{
+        //    return new List<Autor>
+        //    {
+        //        new Autor { Id = 1, Name = "Autor 1" },
+        //        new Autor { Id = 2, Name = "Autor 2" }
+        //    };  
+        //}
+
+        [HttpPost]
+        public async Task<ActionResult> Post (Autor autor) {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Autor>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+        
     }
 }
